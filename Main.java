ec44fa6a04ed701ee8779e0876598d21547334ec
kamaseinu <48120534+kamaseinu@users.noreply.github.com>
2020-01-03 23:00:34 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,46 +11,40 @@ public class Main {
 		boolean end = false;
 		
 		System.out.println("銀行口座管理システムへようこそ！");
-		while (end == false) {
+		while (end == false) {			//条件を満たすまでループ(条件 : end==true)
 			System.out.println("【 登録:1  ログイン:2  終わる:3 】");
-			int action = sc.nextInt();
-			switch (action) {
-			case 1:
-				ac.flag = false;
-				System.out.println("アカウントの登録をします");
-				while (ac.flag == false) {
-					System.out.println("名前を入力してください");
-					String name = sc.next();
-					System.out.println("パスワードを入力してください");
-					String pass = sc.next();
-					ac.Register(name, pass);
+			String action1 = sc.next();
+			if (action1.matches("[0-9]+")) {
+				int action = Integer.parseInt(action1);
+				if (action == 1) {			//1:登録
+					ac.Register();
+				}else if (action == 2) {			//2:ログイン
+					ac.Login();
+				}else if (action == 3) {			//3:終わる
+					System.out.println("ご利用ありがとうございました");
+					end = true;
+				}else {
+					System.out.println("入力が読み取れませんでした");
+					System.out.println("もう一度入力してください");
+					System.out.println("-----------------------------");
 				}
-				break;
-			case 2:
-				ac.flag = false;
-				System.out.println("ログインします");
-				while (ac.flag == false) {
-					System.out.println("IDを入力してください");
-					String ID = sc.next();
-					System.out.println("パスワードを入力してください");
-					String pass = sc.next();
-					ac.Login(ID, pass);
-				}
-				break;
-			case 3:
-				System.out.println("ご利用ありがとうございました");
-				end = true;
-				break;
-			case 0:
+			}else if (action1.equals("hirakegoma")) {			//hirakegoma:保存データの開示 (※隠しコマンド)
+				System.out.println("隠しコマンドが入力されました");
+				System.out.println("保存されているデータを開示します");
+				System.out.print("ID一覧 : \t");
 				System.out.println(ac.ID);
+				System.out.print("パスワード一覧 : \t");
 				System.out.println(ac.Password);
+				System.out.print("残金一覧 : \t");
 				System.out.println(ac.Balance);
-				System.out.println("---------------------------");
-				break;
+				System.out.println("-----------------------------");
+			}else {
+				System.out.println("入力が読み取れませんでした");
+				System.out.println("もう一度入力してください");
+				System.out.println("-----------------------------");
 			}
+			
 		}
 		
-
 	}
-
 }
